7b2f7470c16f89b33f0f828ca720bd85685332f6
balu4007 <balu.ramu1@gmail.com>
2018-10-31 00:44:30 +0530

    added player app template and view

---
+++ player/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from gameplay.models import Game
+
+def home(request):
+    return render(request, "player/home.html",
+                  {'ngames':Game.objects.count()})
