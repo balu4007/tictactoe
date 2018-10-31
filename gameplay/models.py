3142973c1bd0a9ec9d7a74edd87d65d6ee775383
balu4007 <balu.ramu1@gmail.com>
2018-10-31 09:22:00 +0530

    created custom manager and passed date to template from view

--- gameplay/models.py
+++ gameplay/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models import Q
 
 GAME_STATUS_CHOICES=(
     ('F', 'First Player to Move'),
@@ -11,6 +11,13 @@ GAME_STATUS_CHOICES=(
 
 )
 
+
+class GameQuerySet(models.QuerySet):
+    def game_for_user(self,user):
+        return self.filter(Q(first_player=user) | Q(second_player=user))
+    def active_games(self):
+        return self.filter(Q(status='F')|Q(status='S'))
+
 class Game(models.Model):
     first_player=models.ForeignKey(User, related_name="games_first_player", on_delete=models.DO_NOTHING)
     second_player=models.ForeignKey(User, related_name="games_second_player", on_delete=models.DO_NOTHING)
@@ -18,6 +25,8 @@ class Game(models.Model):
     last_action=models.DateTimeField(auto_now=True)
     status=models.CharField(max_length=1, default='F',
                             choices=GAME_STATUS_CHOICES)
+    object = GameQuerySet.as_manager()
+
     def __str__(self):
         return "{0} vs {1}".format(self.first_player,self.second_player)
 
--- player/views.py
+++ player/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from gameplay.models import Game
 
 def home(request):
-    return render(request, "player/home.html",
-                  {'ngames':Game.objects.count()})
+    player_games=Game.object.game_for_user(request.user)
+    active_games=player_games.active_games()
+    return render(request, "player/home.html"
+                  ,{'player_games':active_games})
